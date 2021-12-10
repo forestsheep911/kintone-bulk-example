033e4a2ca2254b0b164b0073122e22ea955811db
徐冰 <forestsheep911@163.com>
2021-12-10 15:37:11 +0800

    ready to fix

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-import { KintoneRestAPIClient } from '@kintone/rest-api-client'
+/*
+ * get bulk records example function
+ * Copyright (c) 2021 Cybozu
+ *
+ * Licensed under the MIT License
+ */
 
-const APP_ID = '16'
-const BASE_URL = 'https://cndevqpofif.cybozu.cn'
-const API_TOKEN = 'DlgJByaoxRaeF3hQlnmj0pgkiEYfAt6uwk0TmNVm'
+// npm 方式：
+// npm i @kintone/rest-api-client
+// import { KintoneRestAPIClient } from '@kintone/rest-api-client'
+// cdn 方式：
+// https://js.cybozu.cn/kintone-rest-api-client/1.4.0/KintoneRestAPIClient.min.js
+
+const BASE_URL = 'https://yourdomain.cybozu.cn'
+const APP_ID = '9999'
+const API_TOKEN = 'yourApiToken'
 
 const client = new KintoneRestAPIClient({
   baseUrl: BASE_URL,
@@ -17,13 +28,85 @@ const client = new KintoneRestAPIClient({
 async function getBulktRecordsViaSDK() {
   const queryParam = {
     app: APP_ID,
-    fields: ['fd_content'],
-    condition: '$id < 501',
-    orderBy: 'fd_content',
+    fields: [''],
+    condition: '',
+    orderBy: '',
     // withCursor: false,
   }
   const result = await client.record.getAllRecords(queryParam)
   return result
+}
+
+// 使用记录ID的方法批量获取数据
+async function getBulktRecordsViaId() {
+  /*
+   * @param {Object} params
+   *   - app {String}: 应用ID（省略时表示当前打开的应用）
+   *   - filterCond {String}: 筛选条件
+   *   - sortConds {Array}: 排序条件的数组
+   *   - fields {Array}: 要获取的字段的数组
+   * @return {Object} response
+   *   - records {Array}: 要获取的记录的数组
+   */
+  async function getRecords(_params) {
+    const MAX_READ_LIMIT = 500
+    const params = _params || {}
+    const app = params.app || kintone.app.getId()
+    const { filterCond } = params
+    const sortConds = params.sortConds || ['$id asc']
+    const { fields } = params
+    let { data } = params
+
+    if (!data) {
+      data = {
+        records: [],
+        lastRecordId: 0,
+      }
+    }
+
+    const conditions = []
+    const limit = MAX_READ_LIMIT
+    if (filterCond) {
+      conditions.push(filterCond)
+    }
+
+    conditions.push(`$id > ${data.lastRecordId}`)
+
+    const sortCondsAndLimit = ` order by ${sortConds.join(', ')} limit ${limit}`
+    const query = conditions.join(' and ') + sortCondsAndLimit
+    const body = {
+      app,
+      query,
+    }
+
+    if (fields && fields.length > 0) {
+      // 因为是按照$id的排序来获取的，因此如果要获取的字段中没有“$id”字段，要追加进去
+      if (fields.indexOf('$id') <= -1) {
+        fields.push('$id')
+      }
+      body.fields = fields
+    }
+
+    const r = await kintone.api(kintone.api.url('/k/v1/records', true), 'GET', body)
+    data.records = data.records.concat(r.records)
+    if (r.records.length === limit) {
+      // 如果获取的记录条数和limit一样，表示可能还有未获取的记录，因此要再回调getRecords，获取剩下的记录
+      data.lastRecordId = r.records[r.records.length - 1].$id.value
+      return getRecords({ app, filterCond, sortConds, fields, data })
+    }
+    delete data.lastRecordId
+    return data
+  }
+
+  const params = {
+    app: APP_ID,
+    filterCond: '',
+    // 排序条件按照['字段代码 asc或者desc']的形式指定
+    sortConds: [''],
+    fields: [''],
+  }
+  const result = await getRecords(params)
+  return result.records
 }
 
 // 使用游标的方法批量获取数据
@@ -94,17 +177,12 @@ async function getBulktRecordsViaCursor() {
     const id = await postCursor(_params)
     return getRecordsByCursorId({ id })
   }
-  /*
-   * call getRecords function sample program
-   * Copyright (c) 2019 Cybozu
-   *
-   * Licensed under the MIT License
-   */
+
   const params = {
     app: APP_ID,
-    filterCond: '$id < 501',
-    sortConds: ['fd_content asc'],
-    fields: ['fd_content'],
+    filterCond: '',
+    sortConds: [''],
+    fields: [''],
   }
   const result = await getRecords(params)
   return result.records
@@ -112,22 +190,98 @@ async function getBulktRecordsViaCursor() {
 
 // 使用offset的方法批量获取数据
 // 一万条以上避免使用
-// 如其他方法可用尽量避免此方法，因为性能较劣
-async function getBulktRecordsViaOffset() {}
+// 如其他方法可用尽量避免此方法，因为性能不占优
+async function getBulktRecordsViaOffset() {
+  /*
+   * @param {Object} _params
+   *   - app {String}: 应用ID（省略时表示当前打开的应用）
+   *   - filterCond {String}: 筛选条件
+   *   - sortConds {Array}: 排序条件的数组
+   *   - fields {Array}: 要获取的字段的数组
+   *   - limit {Number}: 要获取的记录的条数（省略时获取符合筛选条件的所有记录）
+   * @return {Object} response
+   *   - records {Array}: 要获取的记录的数组
+   */
+  async function getRecords(_params) {
+    const MAX_READ_LIMIT = 500
+    const params = _params || {}
+    const app = params.app || kintone.app.getId()
+    const { filterCond } = params
+    const { sortConds } = params
+    const limit = params.limit || -1
+    const offset = params.offset || 0
+    const { fields } = params
+    let { data } = params
+    if (!data) {
+      data = {
+        records: [],
+      }
+    }
+    let willBeDone = false
+    let thisLimit = MAX_READ_LIMIT // 调用getRecords函数的那方指定了记录的获取条数时 //  willBeDone指定为true时，可在获取完指定的条数后退出。
+    if (limit > 0) {
+      if (thisLimit > limit) {
+        thisLimit = limit
+        willBeDone = true
+      }
+    }
+    const conditions = []
+    if (filterCond) {
+      conditions.push(filterCond)
+    }
+    const sortCondsAndLimit = `
+      ${sortConds && sortConds.length > 0 ? ` order by ${sortConds.join(', ')}` : ''} limit ${thisLimit}
+    `
+    const query = `${conditions.join(' and ') + sortCondsAndLimit} offset ${offset}`
+    const body = {
+      app,
+      query,
+    }
+    if (fields && fields.length > 0) {
+      body.fields = fields
+    }
+    const r = await kintone.api(kintone.api.url('/k/v1/records', true), 'GET', body)
+    data.records = data.records.concat(r.records)
+    const offSet = r.records.length
+    if (limit > 0 && limit < offSet) {
+      willBeDone = true
+    } // 获取完该要获取的记录后退出
+    if (offSet < thisLimit || willBeDone) {
+      return data
+    } // 如果该获取的记录还未获取完，再调用函数获取剩余记录
+    return getRecords({
+      app,
+      filterCond,
+      sortConds,
+      limit: limit - offSet,
+      offset: offset + offSet,
+      fields,
+      data,
+    })
+  }
 
-// 使用记录ID的方法批量获取数据
-// 不需要
-async function getBulktRecordsViaId() {}
+  const params = {
+    app: APP_ID,
+    filterCond: '',
+    sortConds: [''],
+    fields: [''],
+  }
+  const result = await getRecords(params)
+  return result.records
+}
+
 kintone.events.on('app.record.index.show', async (event) => {
-  const myContainer = kintone.app.getHeaderSpaceElement()
-  myContainer.innerHTML = '<div class="app"><h1>Hello, kintone!</h1></div>'
   // 1. SDK
-  // const result = await getBulktRecordsViaSDK()
-  // console.log(result.length)
+  const result0 = await getBulktRecordsViaSDK()
+  console.log(result0.length)
+  // 2. 记录ID
+  const result1 = await getBulktRecordsViaId()
+  console.log(result1.length)
   // 2. 游标
-  // const result = await getBulktRecordsViaCursor()
-  // console.log(result.length)
-  const result = await getBulktRecordsViaOffset()
-  console.log(result.length)
+  const result2 = await getBulktRecordsViaCursor()
+  console.log(result2.length)
+  // 4. offset
+  const result3 = await getBulktRecordsViaOffset()
+  console.log(result3.length)
   return event
 })
